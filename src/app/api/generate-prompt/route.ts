f6bed763db05d13580e0997d7195ef3c7594bab2
DevSujal <nimjesujal2004@gmail.com>
2024-10-02 17:40:46 +0530

    Update package-lock.json and package.json

---
+++ src/app/api/generate-prompt/route.ts
@@ -0,0 +1,77 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+import { NextRequest, NextResponse } from "next/server";
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(input) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {
+            text: "extract the ingredient list key words like spicy sugar free etc also find the dishes name something avoid list etc\n\ngive me in these format: \n\ningredients : Array \nkeyWords : Array\ningredientsAvoid : Array\nalso if something else there give to me in the form of variable and value \n\nthese all should be in the form of  json object\n\nhere is the prompt from which you can extract the information:\ni want to make the burger i have vegetables, beef, buns and oil i want spicy and sugar free burger",
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: '```json\n{\n  "ingredients": [\n    "vegetables",\n    "beef",\n    "buns",\n    "oil"\n  ],\n  "keyWords": [\n    "spicy",\n    "sugar free"\n  ],\n  "ingredientsAvoid": [],\n  "dishName": "Burger"\n}\n``` \n',
+          },
+        ],
+      },
+    ],
+  });
+
+  const prompt = `extract the ingredient list key words like spicy sugar free etc also find the dishes name something avoid list etc
+            
+    give me in these format: 
+    ingredients : Array 
+    keyWords : Array
+    ingredientsAvoid : Array
+
+    also if something else there give to me in the form of variable and value
+    
+    give me these all by semi-colan separated values means ingredients : [] ; keywords : [] ; ingredientsAvoid : [] ; all
+    in these format insert the values in the array [] by comma separated values
+    here is the prompt from which you can extract the information:
+    
+    ${input}`;
+
+  console.log("prompt: " + prompt);
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export async function POST(req:NextRequest) {
+  try {
+    const body = await req.json();
+    const { input } = body;
+    const response = await run(input);
+    return NextResponse.json({ response }, { status: 201 });
+  } catch (error) {
+    console.error("Error during generation:", error.message);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,19 +1,39 @@
-import { auth } from "../../auth";
-import Input from "../components/Input"
+"use client";
+import Input from "../components/Input";
+import { useSession } from "next-auth/react";
 import React from "react";
-import { redirect } from "next/navigation";
-const HomePage = async () => {
-  const session = await auth();
-  if (!session) {
-    console.log('session');
-    redirect("/");
+const HomePage = () => {
+  // const session =  useSession();
+  const [input, setInput] = React.useState("");
+  const [response, setResponse] = React.useState([]);
+  async function handleClick() {
+    try {
+      const response = await fetch("/api/generate-prompt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input }),
+      });
+
+      if(!response) {
+        throw new Error("Error during generation");
+      }
+
+      const data = await response.json();
+      const arr = data.response.split(";")
+
+      setResponse(arr);
+    } catch (error) {
+      console.error("Error during generation:", error.message);
+    }
   }
+
   return (
-    <div>
-      <div className="fixed">
-        {console.log(session)}
-      <h1>{session?.user?.name}</h1>
-      {/* <img
+    <div className="flex justify-center items-center flex-col w-full h-full">
+      <div className="fixed top-1 left-1">
+        {/* <h1>{session?.user?.name}</h1> */}
+        {/* <img
         src={session?.user?.image}
         alt={session?.user?.name}
         width={72}
@@ -21,11 +41,29 @@ const HomePage = async () => {
         className="rounded-full"
         />
         */}
-        </div>
-
-      <div className="flex justify-center items-center wfull h-full">
-        <Input />
       </div>
+      <div className="flex flex-col justify-center items-center w-full gap-5">
+        <input
+          type="text"
+          placeholder="enter your prompt..."
+          className="rounded-full p-2 w-2/3 h-10 "
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          onClick={handleClick}
+          className="p-2 text-lg bg-orange-400 rounded w-fit"
+        >
+          click me
+        </button>
+      </div>
+      {response.map((item, index) => {
+        return (
+          <div key={index} className="text-xl">
+            {item}
+          </div>
+        );
+      })}
     </div>
   );
 };
